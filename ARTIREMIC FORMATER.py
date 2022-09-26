8c579ed5027f835267fe1ed489d21858097686be
Nator26-NT <111059939+Nator26-NT@users.noreply.github.com>
2022-09-26 12:51:24 +0200

    Add files via upload

---
+++ ARTIREMIC FORMATER.py
@@ -0,0 +1,16 @@
+
+print("ARTIREMIC FORMATER")
+
+
+num_A  = float(input("  "))
+
+num_B = float(input("  "))
+print("----------------------")
+
+Number_arranger = ([num_A + num_B])
+
+print(Number_arranger)
+
+
+
+
